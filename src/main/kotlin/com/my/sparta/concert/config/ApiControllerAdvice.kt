ec4af2359742557d72d2bf3935dae27823059a5d
진수 <moonlt93@naver.com>
2025-01-03 02:55:27 +0900

    chore: 경로 수정

--- src/main/kotlin/com/my/sparta/concert/config/ApiControllerAdvice.kt
+++ src/main/kotlin/com/my/sparta/concert/config/ApiControllerAdvice.kt
@@ -3,7 +3,6 @@ package com.my.sparta.concert.config
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 

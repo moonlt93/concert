11fc523fccee9f8417ceba323049b3a30592d373
진수 <moonlt93@naver.com>
2025-01-03 08:55:41 +0900

    feat: exception 반환 수정

--- src/main/kotlin/com/my/sparta/concert/config/ApiControllerAdvice.kt
+++ src/main/kotlin/com/my/sparta/concert/config/ApiControllerAdvice.kt
@@ -1,5 +1,6 @@
 package com.my.sparta.concert.config
 
+import org.apache.tomcat.websocket.AuthenticationException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
@@ -15,10 +16,19 @@ class ApiControllerAdvice {
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ErrorResponse {
         return ErrorResponse(
-            HttpStatus.INTERNAL_SERVER_ERROR.name,
+            HttpStatus.INTERNAL_SERVER_ERROR.value().toString(),
             e.message.toString()
         )
     }
+
+    @ExceptionHandler(AuthenticationException::class)
+    fun handleException(e: AuthenticationException): ErrorResponse {
+        return ErrorResponse(
+            HttpStatus.UNAUTHORIZED.value().toString(),
+            e.message.toString()
+        )
+    }
+
 
 }
 
